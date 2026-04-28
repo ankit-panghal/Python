b942d8914e3e6bdd864f8145f8e9a928ddd5620e
Ankit Panghal <ankitpanghal.work@gmail.com>
2026-04-28 11:48:00 +0530

    bis Module

--- main.py
+++ main.py
@@ -131,3 +131,150 @@ for name in liste:
 
 
 #Dictionary------------------------------------------------------
+#Schlüssel-Wert-Paare
+'''person = {
+    'name': 'Alice',
+    'alter': 30,
+    'beruf': 'Entwicklerin',
+    'hobbies': ['Lesen', 'Reisen', 'Kochen']
+}
+
+print(person['name']) # Alice
+print(person['hobbies'][1]) # Reisen
+print(person.keys()) # dict_keys(['name', 'alter', 'beruf', 'hobbies'])
+print(person.values()) # dict_values(['Alice', 30, 'Entwicklerin', ['Lesen', 'Reisen', 'Kochen']])
+person['alter'] = 31 # Aktualisiert das Alter
+person.pop('beruf') # Entfernt den Beruf aus dem Dictionary'''
+
+
+#Funktionen------------------------------------------------------
+'''def begruessung(name='Gast'):
+    print(f'Hallo, {name}! Willkommen zum Python Kurs.')
+
+begruessung() # Hallo, Gast! Willkommen zum Python Kurs.
+begruessung('Alice') # Hallo, Alice! Willkommen zum Python Kurs.'''
+
+'''def berechne_flaeche(radius):
+    pi = 3.14159
+    return pi * radius ** 2
+
+radius = float(input('Geben Sie den Radius des Kreises ein: '))
+flaeche = berechne_flaeche(radius)
+print(f'Die Fläche eines Kreises mit Radius {radius} ist {flaeche}.')'''
+
+'''def addieren(*zahlen):
+    return sum(zahlen)
+
+print(addieren(1, 2, 3)) # 6'''
+
+'''def objekt(**eigenschaften):
+    for key, value in eigenschaften.items():
+        print(f"{key}: {value}")
+    
+objekt(name='Auto', farbe='Rot', marke='Toyota')'''
+
+
+#Tic Tac Toe------------------------------------------------------
+# Spielbrett erstellen
+'''spielbrett = []
+
+def brett_erstellen() :
+    for i in range(3):
+      zeile = [' ' for j in range(3)]
+      spielbrett.append(zeile)
+      
+brett_erstellen()
+# print(spielbrett)
+
+#Spielbrett anzeigen
+def brett_anzeigen() :
+    for i, zeile in enumerate(spielbrett):
+        
+        print('|'.join(zeile))
+        if i < len(spielbrett) -1 : print('-' * 5)
+    
+brett_anzeigen()
+
+def kann_zug_machen(zeile, spalte):
+    return spielbrett[zeile][spalte] == ' '
+
+Züge = {
+    'X': [],
+    'O': []
+}
+
+hatGewonnen = False
+
+def gewinner_pruefen(spieler):
+    for zeile in range(3):
+      if spielbrett[zeile][0] == spieler and spielbrett[zeile][1] == spieler and spielbrett[zeile][2] == spieler:
+         return True
+      if spielbrett[0][zeile] == spieler and spielbrett[1][zeile] == spieler and spielbrett[2][zeile] == spieler:
+         return True
+    
+    if spielbrett[0][0] == spieler and spielbrett[1][1] == spieler and spielbrett[2][2] == spieler:
+         return True
+    if spielbrett[0][2] == spieler and spielbrett[1][1] == spieler and spielbrett[2][0] == spieler:
+         return True
+    return False
+   
+def spieler_zug(spieler):
+    global hatGewonnen
+    while True:
+        zeile = int(input(f'Spieler {spieler}, gib die Zeilennummer (0-2) ein: '))
+        spalte = int(input(f'Spieler {spieler}, gib die Spaltennummer (0-2) ein: '))
+
+        if kann_zug_machen(zeile, spalte):
+            Züge[spieler].append([zeile, spalte])
+            spielbrett[zeile][spalte] = spieler
+            brett_anzeigen()
+            if len(Züge[spieler]) >= 3 :
+              if gewinner_pruefen(spieler) : 
+                 print(f'Spieler {spieler} hat gewonnen!') 
+                 hatGewonnen = True
+                 break
+        else:
+            print('Ungültiger Zug! Versuche es erneut.')
+            continue
+
+        spieler = 'X' if spieler == 'O' else 'O'
+
+
+spieler_zug('X')
+
+def ist_unentschieden() : 
+  for zeile in spielbrett :
+    if ' ' in zeile :
+     return False
+  
+  return True
+   
+if not hatGewonnen and ist_unentschieden() : print('Unentshieden!')'''
+
+#Exception-------------------------------------------------------------
+
+'''try :
+    ergebnis = 100/0
+except ZeroDivisionError:
+    print('Hey, du darfst nicht mit Zero dividieren !')'''
+
+'''try :
+    ergebnis = 100/ '0'
+except TypeError:
+    print('Typen sollen gleich sein !')
+finally :
+    print('Keinen Fehler gemacht!')'''
+
+'''x = 5
+if x >= 5:
+    raise Exception('X soll nicht grosser oder gleich als 5 sein')'''
+
+
+#Module-----------------------------------------------------------------
+#wiederverwendbare Codeteile
+
+'''import willkommen # Code der Datein zugreifen, ohne py
+willkommen.empfang('Ankit')'''
+
+#from math import sqrt #Nur spezifische Funktion importieren, anstatt ganzen Block zu importiern
+
---
+++ willkommen.py
@@ -0,0 +1,3 @@
+
+def empfang(name) :
+    print(f'😊 Hallo {name,} Willkommen zu unserem Event !')
